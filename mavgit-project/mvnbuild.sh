ffde57f8fda2648f36c38b160d8fa33b19f99262
Edwin Chofong <edwinchofong@yahoo.com>
2022-03-01 17:19:49 +0000

    new commit

---
+++ mavgit-project/mvnbuild.sh
@@ -0,0 +1,4 @@
+#mvnbuildscript.sh
+#uncomment the mvn plugin line and define the project parameters to suit your project goals
+#execute the script is project goals and parameters are defined.
+#mvn archetype:generate -DgroupId=.com.mankong-credit-union.bank -DartifactId=villagebank -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
